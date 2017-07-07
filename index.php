967d6e32421fcc55f6dc73b9211e019fb8563e7c
Arcanae <justin.courdesse@gmail.com>
2017-07-07 16:17:02 +0200

    header bootstrap md

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Searching for it !</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php
+    include_once("header.html");
     session_start();
     include_once("database.php");
     if (isset($_SESSION['user'])) {
